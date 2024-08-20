496d0d8add182cebe813ab9a9d2ef48dc87a2a1b
Jose Moises Murillo Rivas <moiesbbuga@gmail.com>
2024-08-20 17:45:12 -0500

    configuramos los servicios para la asignacion de puertos por Rail

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
 import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
-import { init, cleanup } from './whatsapp.js'
-import cors from 'cors'
+import { cleanup, init } from './whatsapp.js'
 
 const app = express()
 
-const host = process.env.WA_SERVER_HOST || undefined
-const port = parseInt(process.env.WA_SERVER_PORT ?? 8000)
+const host = process.env.WA_SERVER_HOST || '0.0.0.0'
+const port = parseInt(process.env.PORT || process.env.WA_SERVER_PORT || 8000)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -17,14 +17,10 @@ app.use('/', routes)
 
 const listenerCallback = () => {
     init()
-    console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
+    console.log(`Server is listening on http://${host}:${port}`)
 }
 
-if (host) {
-    app.listen(port, host, listenerCallback)
-} else {
-    app.listen(port, listenerCallback)
-}
+app.listen(port, host, listenerCallback)
 
 nodeCleanup(cleanup)
 
